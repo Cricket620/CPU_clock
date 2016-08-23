8e1a461658f103150f26ad850d40106a3928cf0f
Cricket620 <caleb.grillo@gmail.com>
2016-08-23 11:58:36 -0400

    Create ___init___.py

---
+++ ___init___.py
@@ -0,0 +1,9 @@
+#Speed of light in m/s
+speed_of_light = 299792458
+
+#One billionth (of a second)
+billionth = 1.0/1000000000
+
+#Nanostick - number of nanometers that a photon in a vacuum travels in one second
+nanostick = speed_of_light*billionth
+
