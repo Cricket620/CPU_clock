3c4e49cf5309f624abd6754c889233bc4af97da3
Cricket620 <caleb.grillo@gmail.com>
2016-08-23 12:15:25 -0400

    Update ___init___.py

--- ___init___.py
+++ ___init___.py
@@ -4,8 +4,8 @@ speed_of_light = 299792458
 #One billionth (of a second)
 billionth = 1.0/1000000000
 
-#Nanostick - number of nanometers that a photon in a vacuum travels in one second
-nanostick = speed_of_light*billionth
+#Nanostick - number of centimeters that a photon in a vacuum travels in one second
+nanostick = speed_of_light*billionth*100
 
 #Processor speed (cycles per second - 2.7GHz used as example)
 cycles_per_second = 2700000000
